5d5ed60a4253dbadf194f1aa1d303413ae5fca59
bruno <bp.nascimento12@gmail.com>
2025-10-14 20:24:36 -0300

    implementação da configuração do railway para a conexão do banco de dados

---
+++ Migrations/20251014231608_initial.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace api_questoes.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Questoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Enunciado = table.Column<string>(type: "text", nullable: false),
+                    Alternativas = table.Column<List<string>>(type: "text[]", nullable: false),
+                    RespostaCorreta = table.Column<string>(type: "text", nullable: false),
+                    Materia = table.Column<string>(type: "text", nullable: false),
+                    NivelDificuldade = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questoes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questoes");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddScoped<IQuestaoService, QuestaoService>();
 
